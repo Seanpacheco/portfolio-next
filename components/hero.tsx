a27c6043b4318e856cff2ddbcbf94c80e254ca32
Sean Pacheco <seanpacheco.code@gmail.com>
2022-12-23 15:58:25 -0500

    add tv hero img

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,11 +6,11 @@ const Hero: FC = () => {
     return(
         <div className="hero min-h-screen bg-[#8E8B9E]">
             <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Hi, I'm Sean</h1>
-                    <p className="py-6">I'm a focused, versatile, software engineer that works great on a team. Have worked on
-various successful front and back-end web applications. Looking to join a tight knit team
-where I can make use of my skills.</p>
+                <div className="max-w-2xl">
+                    <img className='' src="images/old-tv-header.png" alt="old tv" />
+                        <p className="py-6">I'm a focused, versatile, software engineer that works great on a team. Have worked on
+                            various successful front and back-end web applications. Looking to join a tight knit team
+                            where I can make use of my skills.</p>
                 </div>
             </div>
         </div>
