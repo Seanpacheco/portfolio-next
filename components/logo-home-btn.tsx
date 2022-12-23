e9962e8d1c12a4e8d8180c25e3fe6287719005fa
Sean Pacheco <seanpacheco.code@gmail.com>
2022-12-23 13:25:00 -0500

    fix logo padding

--- components/logo-home-btn.tsx
+++ components/logo-home-btn.tsx
@@ -8,7 +8,7 @@ const Logo: FC = () => {
                 <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-7" alt="Flowbite Logo" />
                 <div className='flex flex-col'>
                     <span className="self-center tracking-widest text-4xl font-semibold whitespace-nowrap text-[#797E88]">Sean Pacheco</span>
-                    <div className='flex justify-evenly self-center bg-[#797E88]'><span className=' text-center px-2 text-[#B1B0C0] text-sm tracking-widest'>Full-Stack Software Engineer</span></div>
+                    <div className='flex justify-evenly self-center bg-[#797E88]'><span className=' text-center pt-1 px-2 text-[#B1B0C0] text-sm tracking-widest'>Full-Stack Software Engineer</span></div>
                 </div>
             </a>
         </li>
