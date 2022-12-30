717fb7853b1e86fde0aa628d4bb70c424ccb6622
Sean Pacheco <seanpacheco.code@gmail.com>
2022-12-30 14:45:47 -0500

    hero img optimization

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react'
-
+import Image from 'next/image'
+import tvPic from '../public/images/old-tv-header.png'
 
 
 
@@ -8,7 +9,7 @@ const Hero: FC = () => {
         <div className="hero min-h-screen bg-[#8E8B9E]">
             <div className="hero-content text-center">
                 <div className="max-w-2xl mt-24 lg:mt-0">
-                    <img className='' src="images/old-tv-header.png" alt="old tv" />
+                    <Image className='' priority={true} src={tvPic} alt="old tv" />
                         <p className="py-6 text-2xl">I'm a focused, versatile, software engineer that works great on a team. Have worked on
                             various successful front and back-end web applications. Looking to join a tight knit team
                             where I can make use of my skills.</p>
